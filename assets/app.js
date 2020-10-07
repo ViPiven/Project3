7bfbca79745f2dd7af117a6df987080c9c125579
ViPiven <vitalijjpiven777@gmail.com>
2020-10-07 17:49:56 +0300

    Commit3

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,9 @@ $(document).ready(function (){
     $('.slider').slick({
         arrows:false,
         dots: true,
+        adaptiveHeight: true,
+        infinite: false,
+        mobileFirst: true,
     });
 })
 
